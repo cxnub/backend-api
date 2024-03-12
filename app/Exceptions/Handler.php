8b9e44fb7281999a8eba63786d036ba1ac85c197
cxnub <2302241A@student.tp.edu.sg>
2024-03-13 00:13:14 +0800

    Revert "Added backend api"
    This reverts commit b93345475196dcd30427e5e2396072b5634b52d0.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,6 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -27,9 +26,5 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
-    }
-
-    public function shouldReturnJson($request, Throwable $e) {
-        return str_contains(request()->getRequestUri(), "/api/");
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
+        'name',
         'email',
         'password',
     ];
@@ -41,8 +42,4 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
-
-    public function waters() {
-        return $this->hasMany(Water::class, 'user_id');
-    }
 }
